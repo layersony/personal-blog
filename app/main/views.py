a18b45b173176f35503d8d5326f5a3e17674d494
layersony <sammaingi5@gmail.com>
2021-06-19 19:33:07 +0300

    profile working config

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,8 @@
-from flask import render_template, url_for, redirect
-from flask.templating import render_template_string
+from flask import render_template, url_for, redirect, abort
 from . import main
 from ..models import Blog, User, Comment
 from .forms import CommentForm, BlogPostForm 
+from flask_login import login_required, current_user
 
 @main.route('/')
 def index():
@@ -20,7 +20,17 @@ def postblog():
     
     newblog = Blog(title=title, content=content)
     newblog.save_blog()
-    return redirect(url_for('main.index'))
+    return redirect(url_for('main.profile', uname=current_user.username))
 
   return render_template('profile/postblog.html', blogform=blogform)
 
+@main.route('/user/<uname>')
+def profile(uname):
+  user = User.query.filter_by(username = uname).first()
+
+  
+  if user is None:
+    abort(404)
+  else:
+    blog = Blog.query.filter_by(user_id=current_user.id).all()
+    return render_template('profile/profile.html', user = user, blog = blog)
