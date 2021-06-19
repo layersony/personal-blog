e1965b7a93f19225574889974c059db12bda2fac
layersony <sammaingi5@gmail.com>
2021-06-19 12:31:11 +0300

    initialized databse

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,19 @@
 from flask import Flask
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+
 
 def create_app(config_name):
   app = Flask(__name__)
 
   app.config.from_object(config_options[config_name])
+
+  db.init_app(app)
 
   from .main import main as main_blueprint
   app.register_blueprint(main_blueprint)
