8b92d0e11cc4423cb3cfd190967c0745ab75163b
layersony <sammaingi5@gmail.com>
2021-06-19 14:06:31 +0300

    index diplay blogs

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,11 @@
 from flask import render_template
 from . import main
+from ..models import Blog, User, Comment
+from .forms import CommentForm, BlogPostForm 
 
 @main.route('/')
 def index():
-  heading = 'Working.. Good to go'
-  return render_template('index.html', heading=heading)
+  blog = Blog.get_blogs_content()
+  title = 'Blog'
+  return render_template('index.html', blogs=blog, title=title)
+
