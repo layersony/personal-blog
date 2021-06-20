eac66fdd9a6953136ccc5161af6c409a058c97f6
layersony <sammaingi5@gmail.com>
2021-06-20 07:21:20 +0300

    urllib error done

--- app/request.py
+++ app/request.py
@@ -2,15 +2,20 @@ import urllib.request, json
 from .models import Quote
 
 def get_quotes():
-   with urllib.request.urlopen('http://quotes.stormconsultancy.co.uk/random.json') as url:
-    web_data = url.read()
-    json_data = json.loads(web_data)
+  try:
+    with urllib.request.urlopen('http://quotes.stormconsultancy.co.uk/random.json') as url:
+      web_data = url.read()
+      json_data = json.loads(web_data)
 
-    if json_data.get('id'):
-      author = json_data.get('author')
-      quote = json_data.get('quote')
+      if json_data.get('id'):
+        author = json_data.get('author')
+        quote = json_data.get('quote')
 
+        new_quote = Quote(author, quote)
+        return new_quote
+
+  except urllib.error.URLError:
+      author = 'Maingi Samuel'
+      quote = 'Never Give Up But If you want to Give up just give up the giving up'
       new_quote = Quote(author, quote)
       return new_quote
-    else:
-      return 'No quote At the Moment'
