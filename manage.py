05555c10439a3ed7bd8cb29839c469c38cc7db0a
layersony <sammaingi5@gmail.com>
2021-06-20 21:36:10 +0300

    deploy to heroku

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from flask_script import Manager, Server
 from app.models import Blog, User, Comment
 from flask_migrate import Migrate, MigrateCommand
 
-app = create_app('development')
+app = create_app('production')
 
 migrate = Migrate(app, db)
 manager = Manager(app)
@@ -17,4 +17,3 @@ def make_shell_context():
 
 if __name__ == '__main__':
   manager.run()
-  
